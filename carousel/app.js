e779a4d42e3c90a5f486dd5a5e96984d368dc79c
dikshya-bhandari <dikshyabhandari05@gmail.com>
2021-05-15 18:07:48 +0545

    complete project

--- carousel/app.js
+++ carousel/app.js
@@ -45,22 +45,22 @@ const author  = document.getElementById("author");
 const job  = document.getElementById("job");
 const info =document.getElementById("info");
 
-const prevBtn  = document.getElementById(".prev-btn");
-const nextBtn  = document.getElementById(".next-btn");
-const randomBtn  = document.getElementById(".random-btn");
+const prevBtn  = document.querySelector(".prev-btn");
+const nextBtn  = document.querySelector(".next-btn");
+const randomBtn  = document.querySelector(".random-btn");
 
 
 let currentItem = 0;
 
 //load initial item
 window.addEventListener("DOMContentLoaded" , function(){
-    showPerson(currentItem);
+    showPerson();
 });
 
 //SHOW PERSON BASED ON ITEM
 
-function showPerson(person){
-    const item =reviews[person];
+function showPerson(){
+    const item =reviews[currentItem];
     img.src=item.img;
     author.textContent=item.name;
     job.textContent=item.job;
@@ -70,6 +70,29 @@ function showPerson(person){
 //show next person
 nextBtn.addEventListener("click" , function () {
     currentItem++;
-    showPerson(currentItem);
+    if(currentItem>reviews.length-1){
+        currentItem=0;
+    }
+
+    showPerson();
+})
+
+//show prev person
+prevBtn.addEventListener("click" , function () {
+    currentItem--;
+    if(currentItem<0){
+        currentItem= reviews.length-1;
+    }
+
+    showPerson();
+})
+
+//show random person
+
+randomBtn.addEventListener('click' , function(){
+    currentItem =Math.floor(Math.random() * reviews.length);
+    showPerson();
+
+
 })
 
